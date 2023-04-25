e492ea6c43f3cb68e8a85a5d4716cdedc70e7ec0
Felipeferrazamaral <felipeferrazamaral@gmail.com>
2023-04-24 21:25:23 -0300

    att trabalho

---
+++ pratica2/script.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+// capturando o elemento html
+
+const inputNome = document.getElementById('nome')
+// imprimindo o value do input
+console.log(inputNome.value)
+
+// limpando o input
+
+inputNome.value = ''
+
+const inputEndereco = document.getElementById('endereco')
+console.log(inputEndereco.value)
+inputEndereco.value = ''
+
+const inputEmail = document.getElementById('email')
+console.log(inputEmail.value)
+inputEmail.value = ''
